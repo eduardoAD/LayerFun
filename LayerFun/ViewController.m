fffa2229c5a3c615c9950bae3fc067db69a51e8c
Eduardo Alvarado <e.alvaradod@gmail.com>
2016-02-23 00:01:03 -0600

    first commit

--- LayerFun/ViewController.m
+++ LayerFun/ViewController.m
@@ -16,12 +16,91 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.layer.backgroundColor = [UIColor orangeColor].CGColor;
+    self.view.layer.cornerRadius = 20.0;
+    self.view.layer.frame = CGRectInset(self.view.layer.frame, 20, 20);
+
+    CALayer *sublayer = [CALayer layer];
+    sublayer.backgroundColor = [UIColor blueColor].CGColor;
+    sublayer.shadowOffset = CGSizeMake(0, 3);
+    sublayer.shadowRadius = 5.0;
+    sublayer.shadowColor = [UIColor blackColor].CGColor;
+    sublayer.shadowOpacity = 0.8;
+    sublayer.frame = CGRectMake(30, 30, 128, 192);
+    sublayer.borderColor = [UIColor blackColor].CGColor;
+    sublayer.borderWidth = 2.0;
+    sublayer.cornerRadius = 10.0;
+    [self.view.layer addSublayer:sublayer];
+
+    CALayer *imageLayer = [CALayer layer];
+    imageLayer.frame = sublayer.frame;
+    imageLayer.cornerRadius = 10.0;
+    imageLayer.contents = (id)[UIImage imageNamed:@"BattleMapSplashScreen.jpg"].CGImage;
+    imageLayer.masksToBounds = YES;
+    [self.view.layer addSublayer:imageLayer];
+
+    CALayer *customDrawn = [CALayer layer];
+    customDrawn.delegate = self;
+    customDrawn.backgroundColor = [UIColor greenColor].CGColor;
+    customDrawn.frame = CGRectMake(30, 250, 128, 40);
+    customDrawn.shadowOffset = CGSizeMake(0, 3);
+    customDrawn.shadowRadius = 5.0;
+    customDrawn.shadowColor = [UIColor blackColor].CGColor;
+    customDrawn.shadowOpacity = 0.8;
+    customDrawn.cornerRadius = 10.0;
+    customDrawn.borderColor = [UIColor blackColor].CGColor;
+    customDrawn.borderWidth = 2.0;
+    customDrawn.masksToBounds = YES;
+    [self.view.layer addSublayer:customDrawn];
+    [customDrawn setNeedsDisplay];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+void MyDrawColoredPattern (void *info, CGContextRef context) {
+    CGColorRef dotColor = [UIColor colorWithHue:0 saturation:0 brightness:0.07 alpha:1.0].CGColor;
+    CGColorRef shadowColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.1].CGColor;
+
+    CGContextSetFillColorWithColor(context, dotColor);
+    CGContextSetShadowWithColor(context, CGSizeMake(0, 1), 1, shadowColor);
+
+    CGContextAddArc(context, 3, 3, 4, 0, (CGFloat)M_PI*2, 0);
+    CGContextFillPath(context);
+
+    CGContextAddArc(context, 16, 16, 4, 0, (CGFloat)M_PI*2, 0);
+    CGContextFillPath(context);
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)context {
+
+    CGColorRef bgColor = [UIColor colorWithHue:0.6 saturation:1.0 brightness:1.0 alpha:1.0].CGColor;
+    CGContextSetFillColorWithColor(context, bgColor);
+    CGContextFillRect(context, layer.bounds);
+
+    static const CGPatternCallbacks callbacks = { 0, &MyDrawColoredPattern, NULL };
+
+    CGContextSaveGState(context);
+    CGColorSpaceRef patternSpace = CGColorSpaceCreatePattern(NULL);
+    CGContextSetFillColorSpace(context, patternSpace);
+    CGColorSpaceRelease(patternSpace);
+
+    CGPatternRef pattern = CGPatternCreate(NULL,
+                                           layer.bounds,
+                                           CGAffineTransformIdentity,
+                                           24,
+                                           24,
+                                           kCGPatternTilingConstantSpacing,
+                                           true,
+                                           &callbacks);
+    CGFloat alpha = 1.0;
+    CGContextSetFillPattern(context, pattern, &alpha);
+    CGPatternRelease(pattern);
+    CGContextFillRect(context, layer.bounds);
+    CGContextRestoreGState(context);
 }
 
 @end
